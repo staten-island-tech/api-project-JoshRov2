630e6b03650bc1eb3a11d81aed06dc5b7653659a
JoshRov2 <joshr16@nycstudents.net>
2023-12-07 09:24:20 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,11 +1,12 @@
 import '../css/style.css'
-const URL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/honda?format=json`;
+const URL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json`;
 async function getData(URL){
   try {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data.Results);
-    document.querySelector("h1").textContent = data.textContent;
+    document.querySelector("h1").textContent = data.Mfr;
+    
   } catch (error) {
   }
 };
