1f9cb649cfc8f06541340dad75044ca9c54d500e
JoshRov2 <joshr16@nycstudents.net>
2023-12-08 09:23:48 -0500

    filter

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,13 +1,13 @@
 import '../css/style.css'
-const URL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/bmw?format=json`;
+const URL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json`;
 async function getData(URL){
   try {
     const response = await fetch(URL);
     const data = await response.json();
     console.log(data.Results);
-    document.querySelector("h1").textContent = data.Mfr;
-    
   } catch (error) {
   }
 };
 getData(URL);
+const japanFilter = Results.filter((Manufacturer) => Manufacturer.Country === 'JAPAN'); 
+console.log(japanFilter);
