8b9d054cf0ec2d90750a01b82c53bbe9b3e8b0f5
JoshRov2 <joshr16@nycstudents.net>
2023-12-10 12:27:39 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,13 +1,38 @@
 import '../css/style.css'
-const URL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json`;
-async function getData(URL){
+const ManufacturerURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json`;
+async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.Results);
+    const japanFilter = data.Results.filter((Manufacturer) => Manufacturer.Country === 'JAPAN');
+    printHTML(japanFilter);
+    console.log(japanFilter);
   } catch (error) {
   }
 };
-getData(URL);
-const japanFilter = Results.filter((Manufacturer) => Manufacturer.Country === 'JAPAN'); 
-console.log(japanFilter);
+getData(ManufacturerURL);
+const datatext = document.getElementById('app');
+function printHTML(data) {
+  data.forEach(Manufacturer => {  
+  const VehicleTypes = Manufacturer.VehicleTypes.map((vehicleType)=>
+    vehicleType.Name
+  )
+    const HTML = `
+  <h1>${Manufacturer.Mfr_Name} / ${Manufacturer.Mfr_CommonName}</h1>
+  <h2>${VehicleTypes.join(", ")}</h2>
+  `
+  console.log(VehicleTypes);
+    datatext.insertAdjacentHTML("beforeend", HTML);
+  });
+}
+const MakesURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/bmw?format=json`;
+async function carData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const seriesFilter = data.Results.filter((Manufacturer) => Manufacturer.Model_Name.includes("330"))
+    console.log(seriesFilter);
+  } catch (error) {
+  }
+};
+carData(MakesURL);
