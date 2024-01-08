5f46b017b498b1eed8d779e454c696afcbc97135
JoshRov2 <joshr16@nycstudents.net>
2024-01-08 15:58:23 -0500

    finished

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,59 +1,4 @@
-/* import '../css/style.css'
-const ManufacturerURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetAllManufacturers?format=json`;
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    const countryFilter = data.Results.filter((Manufacturer) => Manufacturer.Country === 'GERMANY');
-    printHTML(countryFilter);
-    console.log(data)
-    console.log(countryFilter);
-  } catch (error) {
-  }
-};
-getData(ManufacturerURL);
- const datatext = document.getElementById('app');
- function printHTML(data) {
-  data.forEach(Manufacturer => {  
-  const VehicleTypes = Manufacturer.VehicleTypes.map((vehicleType)=>
-    vehicleType.Name
-  )
-    const HTML = `
-  <h1>${Manufacturer.Mfr_Name} / ${Manufacturer.Mfr_CommonName}</h1>
-  <h2>${VehicleTypes.join(", ")}</h2>
-  `
-  console.log(VehicleTypes);
-    datatext.insertAdjacentHTML("beforeend", HTML);
-  });
-}
-function printHTML(data) {
-  data.forEach(Manufacturer => {  
-  const VehicleTypes = Model.Model_Name.map((vehicleType)=>
-    vehicleType.Model_Name
-  )
-    const HTML = `
-  <h1>${Manufacturer.Model_Name}/</h1>
-  `
-  console.log(VehicleTypes);
-    maketext.insertAdjacentHTML("beforeend", HTML);
-  })
-};
-const MakesURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/bmw?format=json`;
-async function carData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    const seriesFilter = data.Results.filter((Model) => Model.Model_Name.includes("50i"));
-    console.log(data);
-    console.log(seriesFilter);    
-    printHTML(seriesFilter);
-
-  } catch (error) {
-  }
-};
-carData(MakesURL);
-const maketext = document.getElementById('app');
- */import '../css/style.css'
+import '../css/style.css'
 
 async function dataBase() {
 
@@ -75,13 +20,47 @@ function filterData(results, cb) {
   const filteredData = results.filter(cb)
   return filteredData;
 };
+function startHTML(data) {
+  appEl.textContent = "";
+  const headerHTML = `
+  <div class="notification">
+  <h2>This page will display companies and/or subsidaries that may contain more than one manufacturer. As a result, not every car maker is available here, but specific manufacturers can be found in the model input.</h2>
+  </div>
+  `
+  appEl.insertAdjacentHTML("beforeend", headerHTML)
+  data.forEach((manufacturer) => {
+    let nameTest = manufacturer.Mfr_Name;    
+    const vehicleTypes = manufacturer.VehicleTypes.map((vehicleType) => vehicleType.Name);
+    if (manufacturer.Mfr_CommonName !== null){
+      nameTest = manufacturer.Mfr_CommonName
+    }
+    nameTest = nameTest.toLowerCase().split(" ").join('%20')
+    const HTML = `
+    <div class="manufacturer-card" data-manufacturer=${nameTest}>
+  <h2>${manufacturer.Mfr_Name || ''} / ${manufacturer.Mfr_CommonName || ''}</h2>
+  ${vehicleTypes.length ? `<h3>${vehicleTypes.join(', ')}</h3>` : ""}
+  <button class="detailsButton">View Details</button>
+  <button class="modelsButton">View Models</button>
+    </div>
+    `
+    appEl.insertAdjacentHTML("beforeend", HTML);   
+    
+    const makesURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${nameTest}?format=json`;
+    console.log(makesURL);
+    const detailsURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetManufacturerDetails/${nameTest}?format=json`;
+    console.log(detailsURL);
+  })
+}
+const manuStart = await getData(manufacturerURL);
+
+startHTML(manuStart);
 
 function printHTML(data, countryName) {
   appEl.textContent = "";
   const headerHTML = `
   <div class="notification">
   <h2>Manufacturers based in ${countryName}</h2>
-  <h2>This search parameter will display companies and/or subsidaries that may contain more than one manufacturer. As a result, not every car maker is available here, but specific manufacturers can be found in the model input.</h2>
+  <h2>This page will display companies and/or subsidaries that may contain more than one manufacturer. As a result, not every car maker is available here, but specific manufacturers can be found in the model input.</h2>
   </div>
   `
   appEl.insertAdjacentHTML("beforeend", headerHTML)
@@ -106,14 +85,6 @@ function printHTML(data, countryName) {
     console.log(makesURL);
     const detailsURL = `https://vpic.nhtsa.dot.gov/api/vehicles/GetManufacturerDetails/${nameTest}?format=json`;
     console.log(detailsURL);
-     /* modelsButton.addEventListener("click", () =>{
-      appEl.textContent = "";
-      const makeHTML = `
-       <h2>${makesURL.Make_Name}</h2>
-       <h3>${makesURL.Results}</h3>
-       `
-    appEl.insertAdjacentHTML("beforeend", makeHTML);
-    });  */
   })};
 
   function displayModelsHTML(Results) {
@@ -131,7 +102,7 @@ function printHTML(data, countryName) {
       }else{
         const errorHTML = `
         <div class="error-card">
-        <img src="public/images/checkengine.jpg" id="enginelight" alt="A check engine light.">
+        <img src="/images/checkengine.jpg" id="enginelight" alt="A check engine light.">
         <h2>We've run into an issue...</h2>
         <h3>You have selected a multi-manufacturer company and/or a subsidary name (ex: JAGUAR LAND ROVER NA, LLC). To find the models you are looking for, try the model search input (ex: Jaguar).</h3>
         </div>
@@ -155,15 +126,6 @@ function printHTML(data, countryName) {
   <p>Primary Operator: ${PrincipalFirstName || 'Unavailable'}</p>
   <p>Operator Position: ${PrincipalPosition || 'Unavailable'}</p>
   </div>`
-  /* const manufacturerHTML = `
-  <div class="manu-card">
-  <h3>${car.Mfr_Name}</h3>
-  ${car.Mfr_CommonName ? `<h4>${car.Mfr_CommonName}</h4>` : ''}
-  <p>Country: ${car.Country}</p>
-  <p>City: ${car.City}</p>
-  <p>State/Province: ${car.StateProvince}
-  <p>Address: ${car.Address}</p>
-  </div>` */
   appEl.insertAdjacentHTML('beforeend', manufacturerHTML);
   });
   }
